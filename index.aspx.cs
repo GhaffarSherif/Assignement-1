a3c79a1ee08f9872425aad76e7247950ceaaeae8
Lucas Kourouklis <lucshadow13@hotmail.com>
2016-09-14 21:56:18 -0400

    I don't know why I bother with the message sometimes since this is a school project and we just work on so many things before pushing that a single message is meaningless.

--- index.aspx.cs
+++ index.aspx.cs
@@ -7,10 +7,33 @@ using System.Web.UI.WebControls;
 
 public partial class index : System.Web.UI.Page
 {
+    protected UserDataContext dataContext;
+
     protected void Page_Load(object sender, EventArgs e)
     {
         UnobtrusiveValidationMode = System.Web.UI.UnobtrusiveValidationMode.None;
     }
+
+    protected void loginButton_Click(object sender, EventArgs e)
+    {
+        var enteredUserName = ((TextBox)FindControl("userNameTextBox")).Text;
+        var enteredPassword = ((TextBox)FindControl("passwordTextBox")).Text;
+
+        dataContext = new UserDataContext();
+
+        var loginInfo = from profile in dataContext.Profiles
+                        where profile.Username == enteredUserName &&
+                              profile.Password == enteredPassword
+                        select profile;
+
+        if (loginInfo.Any())
+        {
+            Session["Username"] = enteredUserName;
+            Session["Password"] = enteredPassword;
+            Response.Redirect("~/main.aspx");
+        }
+    }
+
     protected void registerButton_Click(object sender, EventArgs e)
     {
         Response.Redirect("~/register.aspx");
--- main.aspx.cs
+++ main.aspx.cs
@@ -7,8 +7,42 @@ using System.Web.UI.WebControls;
 
 public partial class main : System.Web.UI.Page
 {
+    protected UserDataContext dataContext;
+
     protected void Page_Load(object sender, EventArgs e)
     {
+        UnobtrusiveValidationMode = System.Web.UI.UnobtrusiveValidationMode.None;
 
+        populateProfileTable();
+    }
+
+    public void populateProfileTable()
+    {
+        dataContext = new UserDataContext();
+
+        var users = from profile in dataContext.Profiles
+                         select profile;
+
+        foreach (var user in users)
+            userTableRows.Controls.Add(createRow(user));
+
+        
+    }
+
+    private ProfileTableControl createRow(Profile user)
+    {
+        ProfileTableControl ptControl = (ProfileTableControl)LoadControl("~/ProfileTableControl.ascx");
+
+        ptControl.Username = user.Username;
+        ptControl.viewEvent += new EventHandler(viewEventHandler);
+
+        return ptControl;
+    }
+
+    public void viewEventHandler(object sender, EventArgs e)
+    {
+        string username = (((Button)sender).CommandArgument).ToString();
+
+        Response.Redirect("~/user.aspx?profile="+username);
     }
 }
