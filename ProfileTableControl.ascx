a578190bf118523af2ff908a77a8ace014819088
Lucas Kourouklis <lucshadow13@hotmail.com>
2016-09-14 21:57:24 -0400

    I'm done with writing messages for school projects.

---
+++ ProfileTableControl.ascx
@@ -0,0 +1,6 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="ProfileTableControl.ascx.cs" Inherits="ProfileTableControl" %>
+
+<tr runat="server">
+    <td><%= this.Username %></td>
+    <td><asp:Button runat="server" ID="viewButton" OnClick="viewEventHandler" Text="View User" CausesValidation="false" /></td>
+</tr>
---
+++ ProfileTableControl.ascx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class ProfileTableControl : System.Web.UI.UserControl
+{
+    protected string username;
+
+    public event EventHandler viewEvent = null;
+
+    public string Username
+    {
+        get { return username; }
+        set { username = value; }
+    }
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        viewButton.CommandArgument = Username;
+    }
+
+    public void viewEventHandler(object sender, EventArgs e)
+    {
+        if (viewEvent != null)
+            viewEvent(sender, e);
+    }
+}
